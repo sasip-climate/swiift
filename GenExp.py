88ed4afd059008236d9188897b5493f124bc97bc
Jean-Pierre Auclair <jean-pierre.auclair@univ-grenoble-alpes.fr>
2023-01-27 13:10:44 +0100

    Added ability to create experiments by modifying existing ones (to preserve initial phases particularly)

--- GenExp.py
+++ GenExp.py
@@ -9,6 +9,7 @@ Created on Thu Jan 12 10:49:40 2023
 
 import numpy as np
 import os
+from runpy import run_path
 
 
 def GenExpRand(NRuns, Folder, Vars, Pars):
@@ -97,6 +98,107 @@ def GenExpRand(NRuns, Folder, Vars, Pars):
 
                         os.chdir(f'{cdir}/{Folder}')
     os.chdir(cdir)
+
+
+def ModExp(Folder0, FolderN, Vars, Pars):
+
+    F0s = os.listdir(f'{Folder0}')
+    F0s = [F0 for F0 in F0s if '_mF_' in F0]
+    F0s.sort()
+    NRuns = len(F0s)
+    Nchars = int(np.log10(NRuns)) + 1
+
+    if not os.path.exists(FolderN):
+        os.makedirs(FolderN)
+    cdir = os.getcwd()
+
+    RNG = np.random.default_rng()
+
+    for iRun in np.arange(NRuns):
+        RunN = f'{iRun:0{Nchars}}'
+
+        # Read base file
+        p0 = run_path(f'{Folder0}/{F0s[iRun]}/pars.py')
+
+        # Repeats are a parameter, but never multivalued
+        repeats = Pars['repeats'] if 'repeats' in Pars else p0['repeats']
+
+        # Determine the value of all variables
+        E = drawVal(RNG, Vars['E']) if 'E' in Vars else p0['E']
+        K = drawVal(RNG, Vars['K']) if 'K' in Vars else p0['K']
+        h = drawVal(RNG, Vars['h']) if 'h' in Vars else p0['h']
+        u = drawVal(RNG, Vars['u']) if 'u' in Vars else p0['u']
+        if 'phi0' not in Vars: Vars['phi0'] = [0, 2 * np.pi, 'uniform']
+
+        SpecType = Pars['SpecType'] if 'SpecType' in Pars else p0['SpecType']
+        FracCrit = Pars['FracCrit'] if 'FracCrit' in Pars else p0['FracCrit']
+        tail_fac = Pars['tail_fac'] if 'tail_fac' in Pars else [p0['tail_fac']]
+        multiFrac = Pars['multiFrac'] if 'multiFrac' in Pars else [p0['multiFrac']]
+
+        if 'SpecType' in Pars:
+            if Pars['SpecType'] == p0['SpecType']:
+                phi0 = p0.phi0
+            elif 'Mono' in Pars['SpecType'] and 'Mono' not in p0['SpecType']:
+                phi0 = p0.phi0[5, :]
+            elif 'Mono' not in Pars['SpecType'] and 'Mono' in p0['SpecType']:
+                phi0 = drawVal(RNG, Vars['phi0'], [21, repeats])
+                if 'phi0' in p0: phi0[5, :] = p0['phi0']
+
+        # Loop over parameters for which vars will be shared
+        for ST in SpecType:
+            for FC in FracCrit:
+                for mF in multiFrac:
+                    for tf in tail_fac:
+                        if 'PowerLaw' in ST:
+                            ExpN = f'PowerLaw{ST[1]}_{FC}_mF_{mF}_tf_{tf}'
+                        else:
+                            ExpN = f'{ST[0]}_{FC}_mF_{mF}_tf_{tf}'
+                        if iRun == 0 and not os.path.exists(f'{FolderN}/{ExpN}'):
+                            os.mkdir(f'{FolderN}/{ExpN}')
+
+                        if not os.path.exists(f'{FolderN}/{ExpN}/{ExpN}_{RunN}'):
+                            os.mkdir(f'{FolderN}/{ExpN}/{ExpN}_{RunN}')
+
+                        with open(f'{FolderN}/{ExpN}/{ExpN}_{RunN}/pars.py', 'w') as f:
+                            f.write('#!/usr/bin/env python3\n')
+                            f.write('# -*- coding: utf-8 -*-\n')
+                            f.write('\n')
+                            f.write('import numpy as np\n')
+                            f.write('\n')
+                            f.write('g = 9.8\n')
+                            f.write('rho_w = 1025\n')
+                            f.write('rho_i = 922.5\n')
+                            f.write('\n')
+                            f.write(f'E = {E}\n')
+                            f.write('v = 0.3\n')
+                            f.write(f'K = {K}\n')
+                            f.write('\n')
+                            f.write('strainCrit = 3e-5\n')
+                            f.write('\n')
+                            f.write(f'h = {h}\n')
+                            f.write(f'u = {u}\n')
+                            f.write('f = 0.25\n')
+                            f.write('wl = g / (2 * np.pi * f**2)\n')
+                            f.write(f"SpecType = '{ST[0]}'\n")
+                            f.write(f'n = {ST[1]}\n') if 'Power' in ST[0] else f.write('n = 2\n')
+                            f.write(f'tail_fac = {tf}\n')
+                            f.write('n0 = 1\n')
+                            if 'Mono' in ST[0]:
+                                f.write(f'phi0 = np.{phi0[5,:].__repr__()}\n')
+                            else:
+                                f.write(f'phi0 = np.{phi0.__repr__()}\n')
+                            f.write('\n')
+                            f.write(f"FractureCriterion = '{FC}'\n")
+                            f.write(f'multiFrac = {mF}\n')
+                            f.write(f'repeats = {repeats}\n')
+                            f.write('\n')
+                            f.write('N = 101\n')
+                            f.write('Deriv101 = 6 * np.eye(N) - 4 * np.eye(N, k=1) - 4 * np.eye(N, k=-1) + np.eye(N, k=2) + np.eye(N, k=-2)\n')
+                            f.write('Deriv101[0, [0, 1, 2]] = np.array([2, -4, 2])\n')
+                            f.write('Deriv101[1, [0, 1, 2, 3]] = np.array([-2, 5, -4, 1])\n')
+                            f.write('Deriv101[-1, [-3, -2, -1]] = np.array([2, -4, 2])\n')
+                            f.write('Deriv101[-2, [-4, -3, -2, -1]] = np.array([1, -4, 5, -2])\n')
+                            f.close()
 
 
 def drawVal(RNG, Pars, N=None):
