fd1a914993f827869796383c199ed3cb4fac5df9
Nicolas Mokus <nmks@tuta.io>
2024-04-29 15:55:41 +0200

    TST: add E2E test for curvature, and targets
    Refactor the test for displacement as both tests use the same template

--- tests/test_analytical_energy.py
+++ tests/test_analytical_energy.py
@@ -1,27 +1,46 @@
 #!/usr/bin/env python3
 
+from collections.abc import Callable
 import pathlib
 import numpy as np
 from flexfrac1d.lib.displacement import displacement
+from flexfrac1d.lib.curvature import curvature
 
 # Test configurations visually examined against solution from scipy.solve_bvp
 PATH_DIS = pathlib.Path("tests/target/displacement")
+PATH_CUR = pathlib.Path("tests/target/curvature")
+
+
+def format_to_pack(handle: pathlib.Path) -> tuple[tuple[float], tuple[np.ndarray]]:
+    """Format individual input parameters"""
+    with open(handle, "r") as file:
+        header = file.readline()
+
+    # remove trailing '# ' and split
+    _rn, _lg, *_wv = map(float, header[2:-1].split(","))
+    # format raw floats to output of FloeCoupled._pack
+    _fp = _rn, _lg
+    _wp = tuple(map(np.atleast_1d, (_wv[0], _wv[1] + 1j * _wv[2], _wv[3])))
+    return _fp, _wp
+
+
+def _test_analytical(root_dir: pathlib.Path, func: Callable):
+    sentinel = 0  # make sure no error in path and at least one test was run
+    for handle in root_dir.glob("*ssv"):
+        sentinel += 1
+        loaded = np.loadtxt(handle)
+        # loaded[0]: along-floe space variable x
+        # loaded[1]: reference values for func(x)
+        floe_params, wave_params = format_to_pack(handle)
+
+        # test func(x) against existing displacement
+        assert np.all(loaded[1] == func(loaded[0], floe_params, wave_params))
+    assert sentinel > 0
 
 
 def test_displacement():
-    sentinel = 0  # make sure no error in path and at least one test was run
-    for handle in PATH_DIS.glob("*ssv"):
-        sentinel += 1
-        loaded = np.loadtxt(handle)
-        with open(handle, "r") as file:
-            header = file.readline()
+    _test_analytical(PATH_DIS, displacement)
 
-        # remove trailing '# ' and split
-        _rn, _lg, *_wv = map(float, header[2:-1].split(","))
-        # format raw floats to output of FloeCoupled._pack
-        _fp = _rn, _lg
-        _wp = tuple(map(np.atleast_1d, (_wv[0], _wv[1] + 1j * _wv[2], _wv[3])))
 
-        # test displacement(x) against existing displacement
-        assert np.all(loaded[1] == displacement(loaded[0], _fp, _wp))
-    assert sentinel > 0
+def test_curvature():
+    _test_analytical(PATH_CUR, curvature)
