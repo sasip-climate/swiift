0fd2a0d9a647fe130a7fe33f978f6517f5ec0dc7
Nicolas Mokus <nmks@tuta.io>
2024-07-05 16:21:44 +0200

    MAINT: type hints

--- src/flexfrac1d/lib/_ph_utils.py
+++ src/flexfrac1d/lib/_ph_utils.py
@@ -3,9 +3,11 @@
 import numpy as np
 
 
-def _unit_wavefield(x, c_wavenumbers):
+def _unit_wavefield(x: np.ndarray, c_wavenumbers: np.ndarray) -> np.ndarray:
     return np.exp((1j * c_wavenumbers[:, None]) * x)
 
 
-def _wavefield(x, c_amps, c_wavenumbers):
+def _wavefield(
+    x: np.ndarray, c_amps: np.ndarray, c_wavenumbers: np.ndarray
+) -> np.ndarray:
     return np.imag(c_amps @ _unit_wavefield(x, c_wavenumbers))
