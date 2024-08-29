6e4d34232cc776b2e39510bfaba726eb8331faa1
Nicolas Mokus <nmks@tuta.io>
2024-08-29 20:14:07 +0200

    TST:wip

---
+++ tests/test_graphics.py
@@ -0,0 +1,12 @@
+from unittest import mock
+
+
+def test_segment():
+    # TODO: need a wuf, a bit of a pita to instantiate...
+    pass
+
+
+# @mock.patch(
+#     "flexfrac1d.lib.graphics.plot_displacement",
+# )
+# def test_displacement(): ...
