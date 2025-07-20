fe426836554206fd1dea8aac8655b410cdf195b2
Nicolas Mokus <nmks@tuta.io>
2025-07-20 22:43:53 +0200

    MAINT: rework tests on DiscreteSpectrum
    * Simplify testing. No more handling of weirly shaped input; only deals
      with 0d or 1d sequences.
    * As a consequence, the dependency to primefac is dropped.
    * Valid and invalid shapes passed for initialisation are passed as
      simple pytest parametrisation, instead of relying on hypothesis. We
      thus focus on a short list of explicit examples.
    * Physical strategies are introduced for test on spectra properties, not
      for the logic of shape validation.

--- tests/test_spectrum.py
+++ tests/test_spectrum.py
@@ -1,201 +1,148 @@
-import itertools
-
 from hypothesis import given, strategies as st
-import hypothesis.extra.numpy as npst
 import numpy as np
-import primefac
 import pytest
 
 from swiift.model.model import DiscreteSpectrum
-from tests.conftest import float_kw
+from tests.conftest import physical_strategies
 
-SpectrumParameters = tuple[
-    float | np.ndarray,
-    float | np.ndarray,
-    float | np.ndarray | None,
+# Valid cases: all sizes the same, or some are 1
+valid_cases = [
+    # (amplitude, period, phase)
+    (1, 1, 7),  # (1, 1, 1)
+    ([1], [1], [7]),  # (1, 1, 1)
+    ([1, 2, 9, 6, 1], [1], [7]),  # (5, 1, 1)
+    ([1, 3, 4, 2, 1], [1, 1, 1, 1, 1], 7),  # (5, 5, 1)
+    ([1, 3, 1, 4, 1], [1, 8, 0.1, 1, 1], [7]),  # (5, 5, 1)
+    ([1, 1, 2, 1, 1], [1.5, 1, 0.2, 1, 1], [7, 1, 7, 1, 1]),  # (5, 5, 5)
 ]
-SpectrumParametersKWA = tuple[
-    float | np.ndarray,
-    float | np.ndarray,
-    dict[
-        str,
-        float | np.ndarray,
-    ],
-]
-
-# `test_properties` breaks if max_value is left open (overflow error).
-local_float_kw = float_kw | {"allow_infinity": False, "max_value": 1e9}
-number = st.one_of(
-    st.floats(**local_float_kw),
-    st.integers(),
-)
-
-non_neg_float_kw = local_float_kw | {"min_value": 0}
-non_negative_number = st.one_of(
-    st.floats(**non_neg_float_kw),
-    st.integers(min_value=0),
-)
-
-pos_float_kw = non_neg_float_kw | {"allow_subnormal": False, "exclude_min": True}
-# Use of int max_value to prevent C int overflow problems
-positive_number = st.one_of(
-    st.floats(**pos_float_kw),
-    st.integers(min_value=1, max_value=2**32 - 1),
-)
-
-
-def get_optional_kwargs(*args):
-    combinations = [
-        _c for n in range(len(args) + 1) for _c in itertools.combinations(args, n)
-    ]
-    return st.sampled_from(combinations)
-
-
-@st.composite
-def comp_shapes(draw: st.DrawFn, size: int, max_dims: int | None = None) -> tuple:
-    if size == 0:
-        return tuple()
-    if size in (1, 2, 3, 5, 7, 11, 13, 17, 19, 23):
-        return (size,)
-
-    factors = draw(st.permutations(list(primefac.primefac(size))))
-    if max_dims is None:
-        max_dims = len(factors)
-
-    ndim = draw(st.integers(min_value=1, max_value=max_dims))
-    while len(factors) > ndim:
-        factors[0] = factors[0] * factors[-1]
-        factors = factors[:-1]
-
-    return tuple(factors)
-
-
-@st.composite
-def broadcastable(
-    draw: st.DrawFn,
-) -> SpectrumParametersKWA:
-    size = draw(st.integers(min_value=0, max_value=255))
-    shape_st = comp_shapes(size)
-    ds_kw = dict()
-
-    amplitudes = draw(get_number_or_array(shape_st, False, "pos"))
-    frequencies = draw(get_number_or_array(shape_st, False, "pos"))
-    opt = draw(get_optional_kwargs("phases", "shapes"))
-    if "phases" in opt:
-        ds_kw["phases"] = draw(get_number_or_array(shape_st, False))
-
-    return amplitudes, frequencies, ds_kw
-
-
-def get_number_or_array(
-    shape: st.SearchStrategy | int,
-    strict: bool = False,
-    constraints: str | None = None,
-) -> st.SearchStrategy:
-    if constraints == "non_neg":
-        strategy = npst.arrays(
-            npst.floating_dtypes(sizes=64),
-            shape=shape,
-            elements=non_neg_float_kw,
-        )
-        if not strict:
-            return non_negative_number | strategy
-    elif constraints == "pos":
-        strategy = npst.arrays(
-            npst.floating_dtypes(sizes=64),
-            shape=shape,
-            elements=pos_float_kw,
-        )
-        if not strict:
-            return positive_number | strategy
-    else:
-        strategy = npst.arrays(
-            npst.floating_dtypes(sizes=64),
-            shape=shape,
-            elements=local_float_kw,
-        )
-        if not strict:
-            return number | strategy
-    return strategy
-
-
-@st.composite
-def not_broadcastable(
-    draw: st.DrawFn,
-) -> SpectrumParameters:
-    # 1 is excluded as it will be compatible with anything, and we want to include 0
-    sizes = draw(
-        st.lists(
-            st.just(0) | st.integers(min_value=2, max_value=255),
-            min_size=2,
-            max_size=3,
-            unique=True,
-        )
+valid_shapes = [
+    tuple(
+        [
+            len(subsequence) if isinstance(subsequence, list) else 0
+            for subsequence in sequence
+        ]
     )
-    if len(sizes) == 2:
-        with_phase = draw(st.booleans())
-        if with_phase:
-            idx_scalar = draw(st.integers(min_value=0, max_value=2))
-            if idx_scalar == 0:
-                amplitudes = draw(get_number_or_array(1, constraints="pos"))
-                frequencies = draw(
-                    get_number_or_array(sizes[0], True, constraints="pos")
-                )
-                phases = draw(get_number_or_array(sizes[1], True))
-            elif idx_scalar == 1:
-                amplitudes = draw(
-                    get_number_or_array(sizes[0], True, constraints="pos")
-                )
-                frequencies = draw(get_number_or_array(1, constraints="pos"))
-                phases = draw(get_number_or_array(sizes[1], True))
-            else:
-                amplitudes = draw(
-                    get_number_or_array(sizes[0], True, constraints="pos")
-                )
-                frequencies = draw(
-                    get_number_or_array(sizes[1], True, constraints="pos")
-                )
-                phases = draw(get_number_or_array(1, False))
-        else:
-            amplitudes = draw(get_number_or_array(sizes[0], True, constraints="pos"))
-            frequencies = draw(get_number_or_array(sizes[1], True, constraints="pos"))
-            phases = None
+    for sequence in valid_cases
+]
+
+invalid_cases = [
+    ([1, 1, 1], [1, 1], 1),  # (3, 2, 1)
+    ([1, 1, 1], [1, 1], [1]),  # (3, 2, 1)
+    ([1, 1, 1], [1, 1, 1, 1], [1]),  # (3, 4, 1)
+    ([1, 1, 1], [1, 1], [1, 1, 1]),  # (3, 2, 3)
+    ([1, 1, 1], [1, 1], [1, 1, 1, 1]),  # (3, 2, 4)
+]
+
+
+def parse_shape(
+    size: int, strategy: st.SearchStrategy[float]
+) -> st.SearchStrategy[float | list[float]]:
+    if size == 0:
+        return strategy
     else:
-        amplitudes = draw(get_number_or_array(sizes[0], True, constraints="pos"))
-        frequencies = draw(get_number_or_array(sizes[1], True, constraints="pos"))
-        phases = draw(get_number_or_array(sizes[2], True))
+        return st.lists(
+            strategy,
+            min_size=size,
+            max_size=size,
+        )
 
-    return amplitudes, frequencies, phases
+
+@st.composite
+def spectrum_arguments(
+    draw: st.DrawFn, sizes: tuple[int, int, int]
+) -> list[float | list[float]]:
+    return [
+        draw(parse_shape(size, strategy))
+        for size, strategy in zip(
+            sizes,
+            (
+                physical_strategies["wave"]["amplitude"],
+                physical_strategies["wave"]["frequency"],
+                physical_strategies["wave"]["phase"],
+            ),
+        )
+    ]
 
 
-@given(args=broadcastable())
-def test_sanitised(args: SpectrumParametersKWA):
-    amplitudes, frequencies, kwargs = args
-    spectrum = DiscreteSpectrum(amplitudes, frequencies, **kwargs)
+@st.composite
+def spectrum_arguments_periods(
+    draw: st.DrawFn, sizes: tuple[int, int, int]
+) -> list[float | list[float]]:
+    return [
+        draw(parse_shape(size, strategy))
+        for size, strategy in zip(
+            sizes,
+            (
+                physical_strategies["wave"]["amplitude"],
+                physical_strategies["wave"]["period"],
+                physical_strategies["wave"]["phase"],
+            ),
+        )
+    ]
 
+
+@pytest.mark.parametrize("args", valid_cases)
+@pytest.mark.parametrize("with_shape", (True, False))
+@pytest.mark.parametrize("as_array", (True, False))
+def test_valid_shapes(args, with_shape: bool, as_array: bool):
+    passed_args = args[:-1] if not with_shape else args
+    if as_array:
+        passed_args = (np.asarray(_a) for _a in passed_args)
+    spectrum = DiscreteSpectrum(*passed_args)
     assert spectrum.amplitudes.size > 0
     assert len(spectrum.amplitudes.shape) == 1
     assert spectrum.amplitudes.shape == spectrum.frequencies.shape
     assert spectrum.amplitudes.shape == spectrum.phases.shape
+    assert np.all(spectrum.phases >= 0) and np.all(spectrum.phases < 2 * np.pi)
 
 
-@given(args=not_broadcastable())
-def test_unsanitised(args: SpectrumParameters):
-    amplitudes, frequencies, phases = args
+@pytest.mark.parametrize("args", invalid_cases)
+@pytest.mark.parametrize("with_phase", (True, False))
+@pytest.mark.parametrize("as_array", (True, False))
+def test_invalid_shapes(args, with_phase: bool, as_array: bool):
+    passed_args = args if with_phase else args[:-1]
+    if as_array:
+        passed_args = (np.asarray(_a) for _a in passed_args)
     with pytest.raises(ValueError):
-        if phases is None:
-            DiscreteSpectrum(amplitudes, frequencies)
-        else:
-            DiscreteSpectrum(amplitudes, frequencies, phases)
+        DiscreteSpectrum(*passed_args)
 
 
-@given(args=broadcastable())
+@pytest.mark.parametrize("shapes", valid_shapes)
+@pytest.mark.parametrize("with_phase", (True, False))
+@given(data=st.data())
+def test_from_periods(
+    data: st.DataObject, shapes: tuple[int, int, int], with_phase: bool
+):
+    amplitudes, periods, phases = data.draw(spectrum_arguments_periods(shapes))
+    if with_phase:
+        spectrum = DiscreteSpectrum.from_periods(amplitudes, periods, phases)
+    else:
+        spectrum = DiscreteSpectrum.from_periods(amplitudes, periods)
+    assert np.allclose(1 / spectrum.frequencies, spectrum.periods)
+    # Test frequencies rather than periods, because of floating point errors
+    assert np.isin(
+        1 / np.array(periods), spectrum.frequencies, assume_unique=False
+    ).all()
+
+
+@pytest.mark.parametrize("shapes", valid_shapes)
+@pytest.mark.parametrize("with_phase", (True, False))
 @pytest.mark.parametrize(
     "property", ("periods", "angular_frequencies", "_ang_freqs_pow2", "nf", "energy")
 )
-def test_properties(args, property: str):
-    amplitudes, frequencies, kwargs = args
-    spectrum = DiscreteSpectrum(amplitudes, frequencies, **kwargs)
+@given(data=st.data())
+def test_properties(
+    data: st.DataObject,
+    shapes: tuple[int, int, int],
+    with_phase: bool,
+    property: str,
+):
+    amplitudes, frequencies, phases = data.draw(spectrum_arguments(shapes))
+    if with_phase:
+        spectrum = DiscreteSpectrum(amplitudes, frequencies, phases)
+    else:
+        spectrum = DiscreteSpectrum(amplitudes, frequencies)
+
     if property == "periods":
         assert np.allclose(1 / spectrum.frequencies, spectrum.periods)
     elif property == "angular_frequencies":
@@ -208,8 +155,8 @@ def test_properties(args, property: str):
         )
     elif property == "nf":
         nf = max(np.ravel(amplitudes).size, np.ravel(frequencies).size)
-        if "phases" in kwargs:
-            nf = max(nf, np.ravel(kwargs["phases"]).size)
+        if with_phase:
+            nf = max(nf, np.ravel(phases).size)
         assert nf == spectrum.nf
     elif property == "energy":
         assert np.allclose(np.sum(spectrum.amplitudes**2) / 2, spectrum.energy)
