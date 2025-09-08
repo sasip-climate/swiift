85e283c4e95431ab640ae0909f1511dfb2039e72
Nicolas Mokus <nmks@tuta.io>
2025-09-08 15:13:15 +0200

    ENH: add top-level helper function to compute EP WNs

--- src/swiift/api/utils.py
+++ src/swiift/api/utils.py
@@ -7,12 +7,20 @@ import numpy as np
 from ..lib import dr
 
 if typing.TYPE_CHECKING:
-    from ..model.model import DiscreteSpectrum, Ocean
+    from ..model.model import DiscreteSpectrum, FloatingIce, Ocean
 
 
 def compute_free_surface_wavenumbers(
     ocean: Ocean, spectrum: DiscreteSpectrum, gravity: float
 ) -> np.ndarray:
     solver = dr.FreeSurfaceSolver.from_ocean(ocean, spectrum, gravity)
+    wavenumbers = solver.compute_wavenumbers(real=True)
+    return wavenumbers
+
+
+def compute_elastic_mass_loading_wavenumbers(
+    ice: FloatingIce, spectrum: DiscreteSpectrum, gravity: float
+) -> np.ndarray:
+    solver = dr.ElasticMassLoadingSolver.from_floating(ice, spectrum, gravity)
     wavenumbers = solver.compute_wavenumbers(real=True)
     return wavenumbers
--- tests/test_disp_relations.py
+++ tests/test_disp_relations.py
@@ -1,9 +1,13 @@
 from hypothesis import given, settings
 import numpy as np
 
-from swiift.api.utils import compute_free_surface_wavenumbers
+from swiift.api.utils import (
+    compute_elastic_mass_loading_wavenumbers,
+    compute_free_surface_wavenumbers,
+)
 from swiift.model.model import (
     DiscreteSpectrum,
+    FloatingIce,
     FreeSurfaceWaves,
     Ice,
     Ocean,
@@ -60,4 +64,18 @@ def test_elas_mass_loading(
     x = elas_mass_surface(wui.wavenumbers, ice, ocean, gravity)
     y = angfreqs2 / gravity
     assert np.allclose(x, y)
+
+
 @given(**(coupled_ocean_ice | {"spectrum": spec_mono()}))
+def test_elas_mass_loading_utils(
+    ocean: Ocean, spectrum: DiscreteSpectrum, ice: Ice, gravity: float
+):
+    assert ocean.density > ice.density
+    assert ocean.depth - ice.density / ocean.density * ice.thickness > 0
+    floating_ice = FloatingIce.from_ice_ocean(ice, ocean, gravity)
+    wavenumbers = compute_elastic_mass_loading_wavenumbers(
+        floating_ice, spectrum, gravity
+    )
+    x = elas_mass_surface(wavenumbers, ice, ocean, gravity)
+    y = spectrum._ang_freqs_pow2 / gravity
+    assert np.allclose(x, y)
