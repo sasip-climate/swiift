13b782cba29c430aa625485873edf1cbee37accb
Nicolas Mokus <nmks@tuta.io>
2026-01-08 18:00:39 +1300

    MAINT(fmt)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'swiift'
-copyright = '2026, Nicolas Mokus'
-author = 'Nicolas Mokus'
-release = '0.16.3'
+project = "swiift"
+copyright = "2026, Nicolas Mokus"
+author = "Nicolas Mokus"
+release = "0.16.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
