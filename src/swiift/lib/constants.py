6da1c57ec309f7f9d643473bce33df01b3443833
Nicolas Mokus <nmks@tuta.io>
2026-01-12 15:55:06 +1300

    DOC: add module docstring

--- src/swiift/lib/constants.py
+++ src/swiift/lib/constants.py
@@ -1,4 +1,17 @@
-"""Mathematical constants used through computations."""
+r"""Mathematical constants used through computations.
+
+Attributes
+----------
+PI : float
+    :math:`\pi`
+PI_2 : float
+    :math:`2\pi`
+PI_D4 : float
+    :math:`\frac{\pi}{4}`
+SQR2 : float
+    :math:`\sqrt{2}`
+
+"""
 
 from math import pi, sqrt
 
