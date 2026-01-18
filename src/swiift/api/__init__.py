3e721ea738d3c8af8d59cadb434628667ad37f15
Nicolas Mokus <nmks@tuta.io>
2026-01-19 12:26:04 +1300

    BUG: fix imports to preserve backward compatibility
    We aim to expose a selection of members at the package top-level.
    However, theses members should still be accessible explicitely through
    their complete path. For example, `swiift.Experiment` should work, but
    `swiift.api.api.Experiment` should work as well. The latter didn't in
    the previous implementation, this is fixed by the present commit.

--- src/swiift/api/__init__.py
+++ src/swiift/api/__init__.py
@@ -1,5 +1,11 @@
 """TODO"""
 
+from . import spectra, utils
 from .api import Experiment, load_pickle, load_pickles
 
-__all__ = [s for s in dir() if not s.startswith("_")]
+# Explicitly expose the members of the module `api.py`.
+# Exclude the module itself to avoid shadowing the subpackage `api/`;
+# the namespace will however exists due to the `from .api` import.
+# The namespaces associated with the two other submodules are added explicitly,
+# so they can be added to a higher-level namespace.
+__all__ = [s for s in dir() if not s.startswith("_") and s != "api"]
